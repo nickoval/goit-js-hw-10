b7d0344168a8b9f46f7e8e905268446f371efb6b
nickoval <85960879+nickoval@users.noreply.github.com>
2022-11-03 12:20:12 +0000

    Deploying to gh-pages from @ nickoval/goit-js-hw-10@c1740cd2da26e9cf513bf0bc922d572582b4e25a 🚀

--- index.c72ad67e.js
+++
@@ -1,2 +0,0 @@
-!function(){document.querySelector("#search-box"),document.querySelector(".country-list"),document.querySelector(".country-info");fetch("https://restcountries.com/v3.1/name/".concat("sw","?fields=name.official,capital,population,flags.svg,languages")).then((function(o){o.json(),console.log(o)})).catch((function(o){console.log(o)}))}();
-//# sourceMappingURL=index.c72ad67e.js.map
